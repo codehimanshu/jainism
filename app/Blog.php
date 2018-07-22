999de4824b203da3af05c40d3e166d8eb3ce6ef9
codehimanshu <himanshuagrawal1998@gmail.com>
2018-07-22 21:50:26 +0530

    add blog models

---
+++ app/Blog.php
@@ -0,0 +1,13 @@
+<?php
+
+namespace App;
+
+use Illuminate\Database\Eloquent\Model;
+
+class Blog extends Model
+{
+    protected $fillable = [
+    	'id', 'title', 'author', 'content', 'source', 'image'
+    ];
+}
+}
